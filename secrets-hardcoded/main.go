ca82c6f18f699667e5dbc097103ee1b89fce32c6
Alex Ilgayev <alex@cycode.com>
2023-01-15 23:41:00 +0200

    update

---
+++ secrets-hardcoded/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+var (
+	AWS_ACCESS_KEY_ID     = "AKIAUDSMZPA6FOJTM5MJ"
+	AWS_SECRET_ACCESS_KEY = "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"
+)
+
+// Do stuff with AWS credentials
+
+func HandleRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+}
+
+func main() {
+	http.HandleFunc("/", HandleRoot)
+	fmt.Println("Starting a simple go server on port 8080")
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
