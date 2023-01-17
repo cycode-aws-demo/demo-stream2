21887f7587e1ea1fdf7b02ed393f6e8e1775156e
Alex Ilgayev <alex@cycode.com>
2023-01-17 13:25:45 +0200

    removing secret

--- secrets-hardcoded/main.go
+++ secrets-hardcoded/main.go
@@ -6,11 +6,6 @@ import (
 	"net/http"
 )
 
-var (
-	AWS_ACCESS_KEY_ID     = "AKIAUDSMZPA6FOJTM5MJ"
-	AWS_SECRET_ACCESS_KEY = "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"
-)
-
 // Do stuff with AWS credentials
 
 func HandleRoot(w http.ResponseWriter, r *http.Request) {
